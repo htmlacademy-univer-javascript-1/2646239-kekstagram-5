3146ebebc664ec8e4a2e15b39f97d4c45e2fd5d1
Mikhail <eremix2017@yandex.ru>
2025-05-17 14:58:00 +0300

    task 8.14 refactoring

--- js/fullview.js
+++ js/fullview.js
@@ -1,50 +1,56 @@
-const linkPost = function(bigPictureNode, element) {
-  const postImg = bigPictureNode.querySelector('.big-picture__img > img');
-  const postCaption = bigPictureNode.querySelector('.social__caption');
-  const postLikesCount = bigPictureNode.querySelector('.likes-count');
-  document.body.classList.add('modal-open');
+const bigPictureNode = document.querySelector('.big-picture');
+const bigPictureCancel = document.querySelector('.big-picture__cancel');
 
-  postImg.src = element.url;
-  postCaption.textContent = element.description;
-  postLikesCount.textContent = element.likes.toString();
+const linkComment = function(commentTemplate, comment, fragment) {
+  const commentNode = commentTemplate.cloneNode(true);
+  const commentAvatar = commentNode.querySelector('img');
+  const commentMessage = commentNode.querySelector('p');
+  commentAvatar.src = comment.avatar;
+  commentMessage.textContent = comment.message;
+  fragment.appendChild(commentNode);
+};
 
-
+const linkPostComments = function(element) {
   const commentList = bigPictureNode.querySelector('.social__comments');
   const fragment = document.createDocumentFragment();
   const commentTemplate = document.querySelector('#comment').content;
 
   element.comments.forEach((comment) => {
-    const commentNode = commentTemplate.cloneNode(true);
-    const commentAvatar = commentNode.querySelector('img');
-    const commentMessage = commentNode.querySelector('p');
-    commentAvatar.src = comment.avatar;
-    commentMessage.textContent = comment.message;
-    fragment.appendChild(commentNode);
+    linkComment(commentTemplate, comment, fragment);
   });
 
   commentList.innerHTML = '';
   commentList.appendChild(fragment);
-
 };
 
-export const openFullPhotoView = function(element) {
+const linkPostImg = function(element) {
+  const postImg = bigPictureNode.querySelector('.big-picture__img > img');
+  const postCaption = bigPictureNode.querySelector('.social__caption');
+  const postLikesCount = bigPictureNode.querySelector('.likes-count');
+  postImg.src = element.url;
+  postCaption.textContent = element.description;
+  postLikesCount.textContent = element.likes.toString();
+};
 
-  const bigPictureNode = document.querySelector('.big-picture');
-  const bigPictureCancel = document.querySelector('.big-picture__cancel');
+const linkPostData = function(element) {
+  linkPostImg(element);
+  linkPostComments(element);
+};
 
+const handleCloseButton = function() {
   bigPictureCancel.addEventListener('click', (evt) => {
     evt.preventDefault();
     bigPictureNode.classList.add('hidden');
     document.body.classList.remove('modal-open');
   });
+};
 
+export const openFullPhotoView = function(element) {
+  document.body.classList.add('modal-open');
   bigPictureNode.classList.remove('hidden');
 
-  linkPost(bigPictureNode, element);
-
-  // eslint-disable-next-line no-console
-  console.log(element);
-
+  handleCloseButton();
+  linkPostData(element);
 };
 
 
